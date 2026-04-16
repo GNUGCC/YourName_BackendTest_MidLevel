b590ec1b5da94a4b05c9d12a850727f2431f6d39
JeffreyLiu <>
2026-04-16 13:44:06 +0800

    init

--- YourName_BackendTest_MidLevel/Program.cs
+++ YourName_BackendTest_MidLevel/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddScoped<IDbConnection>(_ => new SqlConnection("(local)\\mssqllocaldb"));
+builder.Services.AddScoped<IDbConnection>(_ => new SqlConnection("server=localhost;database=BackendExamHub;Trusted_Connection=True;TrustServerCertificate=True;"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ YourName_BackendTest_MidLevel/Services/MyOfficeAcpdService.cs
@@ -0,0 +1,17 @@
+﻿using Dapper;
+using System.Data;
+
+namespace BackendTest_MidLevel.Services;
+
+public class MyOfficeAcpdService
+{
+    public static async Task<string> GetNewSid(IDbConnection dbConnection)
+    {
+        var parameters = new DynamicParameters();
+        parameters.Add("@TableName", "Test");
+        parameters.Add("@ReturnSID", dbType: DbType.String, size: 20, direction: ParameterDirection.Output);
+
+        await dbConnection.ExecuteAsync("NEWSID", parameters, commandType: CommandType.StoredProcedure);
+        return parameters.Get<string>("@ReturnSID");
+    }
+}
